87fe6eb129345c9819f3513b42e43a9579838ba9
Rohansie K.D.S. <it24102056@my.sliit.lk>
2025-05-20 03:58:06 +0530

    Update LoginSelectionServlet.java

--- src/main/java/com/inventory/LoginSelectionServlet.java
+++ src/main/java/com/inventory/LoginSelectionServlet.java
@@ -13,11 +13,11 @@ public class LoginSelectionServlet extends HttpServlet {
         String role = request.getParameter("role");
 
         if ("admin".equals(role)) {
-            response.sendRedirect("adminLogin.jsp");
+            response.sendRedirect("adminLogin");
         } else if ("supplier".equals(role)) {
             response.sendRedirect("supplierLogin.jsp");
-        } else if ("user".equals(role)) {
-            response.sendRedirect("login.jsp");
+        } else if ("buyer".equals(role)) {
+            response.sendRedirect("buyerLogin");
         } else {
             response.sendRedirect("index.jsp"); // Redirect to home if role is not recognized
         }
